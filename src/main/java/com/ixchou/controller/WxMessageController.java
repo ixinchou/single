8ba22d3505a26d7eccbf30c311a0cd21add39c3c
Hsiang Leekwok <xfeiffer@hotmail.com>
2019-12-25 23:27:18 +0800

    增加微信消息推送处理controller

---
+++ src/main/java/com/ixchou/controller/WxMessageController.java
@@ -0,0 +1,16 @@
+package com.ixchou.controller;
+
+/**
+ * <b>Author</b>: Hsiang Leekwok<br/>
+ * <b>Date</b>: 2019/12/25 22:47<br/>
+ * <b>Version</b>: v1.0<br/>
+ * <b>Subject</b>: <br/>
+ * <b>Description</b>:
+ */
+public class WxMessageController {
+    /**
+     *   url: https://edu.ixchou.com/message/pull
+     * token: 92c8b2d368204645a7420d9e5889ddd8
+     *   key: NU9Zhmm72snqUjJZ5dKuYX0909KvtoVVm4K95lbahRE
+     */
+}
